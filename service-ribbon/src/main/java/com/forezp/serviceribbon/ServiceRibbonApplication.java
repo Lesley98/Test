d3c06e4cb935282b18463684352df51b849e8e3a
zhanglu <1515605401@qq.com>
2019-08-13 16:27:38 +0800

    the 4 commit

--- service-ribbon/src/main/java/com/forezp/serviceribbon/ServiceRibbonApplication.java
+++ service-ribbon/src/main/java/com/forezp/serviceribbon/ServiceRibbonApplication.java
@@ -7,13 +7,14 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
+//version 2.0
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
 public class ServiceRibbonApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run( ServiceRibbonApplication.class, args );
     }
 
